eaba5d8b58e0a1a002f049bdd42acccb39263309
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-02 16:59:47 +0900

    feat(Location) : 내위치 버튼 퍼블리싱

--- src/app/area/page.tsx
+++ src/app/area/page.tsx
@@ -1,3 +1,12 @@
 export default function AreaPage() {
-  return <div>AreaPage</div>;
+  return (
+  <div className="bg-areabg">
+    <div className="flex justify-end">
+      <label className="text-[11px] leading-[13px] align-top mx-[5px] my-0 text-black">내위치</label>
+      <button className="w-[22px] h-[13px] relative bg-gray rounded-3xl">
+        <div className="w-[11px] h-[11px] bg-white rounded-[50%]"></div>
+      </button>
+    </div>
+  </div>
+  );
 }
