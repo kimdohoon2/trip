cd97a332e251778bca5a1481443bdd951a045682
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-02 17:36:59 +0900

    bug(prettier) : 태일윈드를 설치를한해서 프리티어가 안먹혀서 설치추가

--- src/app/area/page.tsx
+++ src/app/area/page.tsx
@@ -1,12 +1,14 @@
-export default function AreaPage() {
-  return (
-  <div className="bg-areabg">
-    <div className="flex justify-end">
-      <label className="text-[11px] leading-[13px] align-top mx-[5px] my-0 text-black">내위치</label>
-      <button className="w-[22px] h-[13px] relative bg-gray rounded-3xl">
-        <div className="w-[11px] h-[11px] bg-white rounded-[50%]"></div>
-      </button>
+export default function AreaPage() {   return (
+    <div className="bg-areabg">
+      <div className="flex justify-end">
+        <label className="mx-[5px] my-0 align-top text-[11px] leading-[13px] text-black">
+          내위치
+        </label>
+        <button className="relative h-[13px] w-[22px] rounded-3xl bg-gray">
+          <div className="h-[11px] w-[11px] rounded-[50%] bg-white"></div>
+          <div></div>
+        </button>
+      </div>
     </div>
-  </div>
   );
 }
