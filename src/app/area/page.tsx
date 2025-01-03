ff9094edf832be19fb35f517712efffb99e9617b
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-03 19:18:39 +0900

    feat(지역)  : 지역페이지

--- src/app/area/page.tsx
+++ src/app/area/page.tsx
@@ -1,14 +1,9 @@
-export default function AreaPage() {   return (
-    <div className="bg-areabg">
-      <div className="flex justify-end">
-        <label className="mx-[5px] my-0 align-top text-[11px] leading-[13px] text-black">
-          내위치
-        </label>
-        <button className="relative h-[13px] w-[22px] rounded-3xl bg-gray">
-          <div className="h-[11px] w-[11px] rounded-[50%] bg-white"></div>
-          <div></div>
-        </button>
-      </div>
-    </div>
+import AreaHeader from '../components/Area/AreaHeader';
+
+export default function AreaPage() {
+  return (
+    <>
+      <AreaHeader />
+    </>
   );
 }
