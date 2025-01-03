eeafd4f36faff4fb9d6ae7467e6ec1a1d87fa2d2
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-03 19:15:13 +0900

    feat(type) : 타입정의

---
+++ src/app/type/ItemType.ts
@@ -0,0 +1,10 @@
+export interface Item {
+  code: string[];
+  name: string[];
+  rnum: string[];
+}
+
+export interface AreaItem {
+  code: string;
+  name: string;
+}
--- src/app/type/type.tsx
+++
@@ -1,6 +0,0 @@
-export type TourItem = {
-  title: string;
-  addr1: string;
-  firstimage: string;
-  firstimage2: string;
-};
