519012d6248289f9bb8802c0c5d0590451549f4e
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-07 10:37:21 +0900

    feat(Header): css변경

--- src/app/components/Area/AreaHeader.tsx
+++ src/app/components/Area/AreaHeader.tsx
@@ -2,7 +2,7 @@ import AreaSlide from './AreaSlide';
 
 export default function AreaHeader() {
   return (
-    <div className="bg-areabg pb-10 pt-3">
+    <div className="bg-areabg pb-8 pt-3">
       <div className="relative right-5 mb-2 flex items-center justify-end">
         <label className="mx-[5px] text-[11px] text-black">내위치</label>
         <button className="relative h-[13px] w-[22px] rounded-3xl bg-gray">
