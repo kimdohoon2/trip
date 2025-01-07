d0adb8cc01f9121943d18ecf3ad1dce624ca9675
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-07 11:09:19 +0900

    feat(modal): 좋아요를 다시 클릭하면 취소하는 로직추가

--- src/app/components/Common/modal.tsx
+++ src/app/components/Common/modal.tsx
@@ -4,9 +4,17 @@ interface ModalProps {
   isOpen: boolean;
   message: string;
   onClose: () => void;
+  onConfirm?: () => void;
+  confirmOnly?: boolean;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, message, onClose }) => {
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  message,
+  onClose,
+  onConfirm,
+  confirmOnly = false,
+}) => {
   if (!isOpen) return null;
 
   // 배경 클릭 시 모달을 닫을 수 있도록 하는 함수
@@ -18,17 +26,29 @@ const Modal: React.FC<ModalProps> = ({ isOpen, message, onClose }) => {
 
   return (
     <div
-      className="bg-bgopacity fixed inset-0 z-50 flex items-center justify-center"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-bgopacity"
       onClick={handleBackgroundClick}
     >
       <div className="rounded-lg bg-white p-6 shadow-lg">
         <p className="text-center text-lg font-bold">{message}</p>
-        <button
-          onClick={onClose}
-          className="mt-4 block w-full rounded-lg bg-blue-500 py-2 text-center text-white"
-        >
-          닫기
-        </button>
+        <div className="mt-4 flex gap-4">
+          {/* 확인 버튼 */}
+          <button
+            onClick={onConfirm || onClose}
+            className="flex-1 rounded-lg bg-blue-500 py-2 text-center text-white"
+          >
+            확인
+          </button>
+          {/* 닫기(취소) 버튼 (확인 버튼만 보이는 경우 제외) */}
+          {!confirmOnly && (
+            <button
+              onClick={onClose}
+              className="flex-1 rounded-lg bg-gray py-2 text-center text-white"
+            >
+              취소
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
