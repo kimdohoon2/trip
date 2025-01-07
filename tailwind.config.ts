792a97549ae3260669034ab6bbf42eb3747e322d
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-07 10:36:42 +0900

    feat(css) : css추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        white: "var(--white)",
-        black: "var(--black)",
-        textcolor: "var(--textcolor)",
-        lightorange: "var(--lightorange)",
-        bordercolor: "var(--bordercolor)",
-        gray: "var(--gray)",
-        custompink: "var(--custompink)",
-        customyellow: "var(--customyellow)",
-        customskyblue: "var(--customskyblue)",
-        areabg: "var(--areabg)",
+        white: 'var(--white)',
+        black: 'var(--black)',
+        textcolor: 'var(--textcolor)',
+        lightorange: 'var(--lightorange)',
+        bordercolor: 'var(--bordercolor)',
+        gray: 'var(--gray)',
+        custompink: 'var(--custompink)',
+        customyellow: 'var(--customyellow)',
+        customskyblue: 'var(--customskyblue)',
+        areabg: 'var(--areabg)',
+        bgopacity: 'var(--bgopacity)',
       },
       animation: {
-        progress: "progress_ani 3s linear",
-        rotate_image: "rotate_image 20s linear infinite",
+        progress: 'progress_ani 3s linear',
+        rotate_image: 'rotate_image 20s linear infinite',
       },
       keyframes: {
         progress_ani: {
-          "0%": { transform: "scaleX(0)" },
-          "100%": { transform: "scaleX(1)" },
+          '0%': { transform: 'scaleX(0)' },
+          '100%': { transform: 'scaleX(1)' },
         },
         rotate_image: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
         },
       },
     },
