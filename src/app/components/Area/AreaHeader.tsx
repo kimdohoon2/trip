f93271b601f5f5b061e9a66ac6fc067fd2413eb1
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-08 22:27:43 +0900

    feat(conponents) : 부모컴퍼넌트에서 props받기, pc까지 퍼블리싱 완료

--- src/app/components/Area/AreaHeader.tsx
+++ src/app/components/Area/AreaHeader.tsx
@@ -1,15 +1,16 @@
 import AreaSlide from './AreaSlide';
+import { AreaSlideProps } from '@/app/type/ItemType';
 
-export default function AreaHeader() {
+export default function AreaHeader({ selectedArea, setSelectedArea }: AreaSlideProps) {
   return (
-    <div className="bg-areabg pb-8 pt-3">
-      <div className="relative right-5 mb-2 flex items-center justify-end">
-        <label className="mx-[5px] text-[11px] text-black">내위치</label>
-        <button className="relative h-[13px] w-[22px] rounded-3xl bg-gray">
-          <div className="h-[11px] w-[11px] rounded-[50%] bg-white"></div>
+    <div className="1xl:pt-20 bg-areabg pb-8 pt-3 lg:pb-0 lg:pt-28">
+      <div className="1xl:right-16 relative right-5 mb-2 flex items-center justify-end lg:right-7">
+        <label className="mx-[5px] text-[11px] text-black lg:text-base">내위치</label>
+        <button className="relative h-[13px] w-[22px] rounded-3xl bg-gray lg:h-[15px] lg:w-[30px]">
+          <div className="h-[13px] w-[13px] rounded-[50%] bg-white lg:h-[15px] lg:w-[15px]"></div>
         </button>
       </div>
-      <AreaSlide />
+      <AreaSlide selectedArea={selectedArea} setSelectedArea={setSelectedArea} />
     </div>
   );
 }
