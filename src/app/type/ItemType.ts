3513fe5e79e0a01633eb9108454f0c3e21e3093c
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-08 22:06:40 +0900

    feat(type) : 타입정리

--- src/app/type/ItemType.ts
+++ src/app/type/ItemType.ts
@@ -7,4 +7,27 @@ export interface AreaItem {
   firstimage2: string; // 썸네일 이미지 URL
   contenttypeid: string; // 컨텐츠 타입 ID
   contentid: string; // 컨텐츠 ID
+  areaCode: string; // 지역 코드
+}
+
+export interface AreaContentsProps {
+  tourData: AreaItem[];
+  loading: boolean;
+  error: string | null;
+  selectedArea: string;
+}
+
+export interface AreaSlideProps {
+  selectedArea: string;
+  setSelectedArea: (area: string) => void;
+}
+export interface MainSlideType {
+  title: string;
+  image: string;
+  description: string;
+  description2?: string;
+}
+export interface AreaSlideType {
+  title: string;
+  image: string;
 }
