ebe068f176a94a6aee1c31fe22d3566ff0f4c765
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-01-10 00:14:02 +0900

    feat(AreaHeader) : css 추가

--- src/app/components/Area/AreaHeader.tsx
+++ src/app/components/Area/AreaHeader.tsx
@@ -3,8 +3,8 @@ import { AreaSlideProps } from '@/app/type/ItemType';
 
 export default function AreaHeader({ selectedArea, setSelectedArea }: AreaSlideProps) {
   return (
-    <div className="1xl:pt-20 bg-areabg pb-8 pt-3 lg:pb-0 lg:pt-28">
-      <div className="1xl:right-16 relative right-5 mb-2 flex items-center justify-end lg:right-7">
+    <div className="bg-areabg pb-8 pt-3 lg:pb-0 lg:pt-28 1xl:pt-20">
+      <div className="relative right-5 mb-2 flex items-center justify-end lg:right-7 1xl:right-16">
         <label className="mx-[5px] text-[11px] text-black lg:text-base">내위치</label>
         <button className="relative h-[13px] w-[22px] rounded-3xl bg-gray lg:h-[15px] lg:w-[30px]">
           <div className="h-[13px] w-[13px] rounded-[50%] bg-white lg:h-[15px] lg:w-[15px]"></div>
