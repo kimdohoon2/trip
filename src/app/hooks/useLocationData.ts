3ac26b9abff5fe484983999e0669809d254d17b6
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-14 20:21:27 +0900

    refactory: api분리에 따른 import 경로 변경완료

--- src/app/hooks/useLocationData.ts
+++ src/app/hooks/useLocationData.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { LocationApi } from '@/app/api/tourApi';
+import { getLocationApi } from '@/app/api/getLocationApi';
 import { LocationApiResponse } from '@/app/types/ItemType';
 
 export function useLocationData() {
   return useQuery<LocationApiResponse, Error>({
     queryKey: ['locationData'],
-    queryFn: LocationApi,
+    queryFn: getLocationApi,
     staleTime: 5 * 60 * 1000,
     retry: 1,
     enabled: false,
--- src/app/hooks/useTourData.ts
+++ src/app/hooks/useTourData.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { TourListApi } from '@/app/api/tourApi';
+import { getTourListApi } from '@/app/api/getTourListApi';
 import { AreaItem } from '@/app/types/ItemType';
 
 export function useTourData(selectedArea: string) {
   return useQuery<AreaItem[], Error>({
     queryKey: ['tourData', selectedArea],
-    queryFn: () => TourListApi(selectedArea),
+    queryFn: () => getTourListApi(selectedArea),
     staleTime: 5 * 60 * 1000, // 5분
     retry: 1,
   });
