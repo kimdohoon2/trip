7414b74f10d1c7a89e8b6ae634f39bd4dad939a1
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-18 10:47:55 +0900

    refactory : 숙박컨텐츠 컴퍼넌트 분리 완료
    - 가독성 향상
    - 컴퍼넌트가 크기 및 재사용성 증가

--- src/app/components/Accomdation/AccomdationContents.tsx
+++ src/app/components/Accomdation/AccomdationContents.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { useState } from 'react';
 import Image from 'next/image';
-import { AreaHeaderSlide } from '@/app/constant/SlideConstant';
 import { useStayData } from '@/app/hooks/useStayData';
 import { useInteractionStore } from '@/app/stores/useInteractionStore';
 import ProgressBar from '@/app/components/Common/ProgressBar';
@@ -11,36 +9,17 @@ import MoreButton from '@/app/components/Common/MoreButton';
 import Spinner from '@/app/components/Common/Spinner';
 import DataError from '@/app/components/Common/Error';
 import AreaEmptyState from '@/app/components/Common/AreaEmptyState';
-import { StayItem } from '@/app/types/ItemType';
+import AccomdationHeader from '@/app/components/Accomdation/AccomdationHeader';
+import { useStayProcessor } from '@/app/hooks/useStayProcessor';
+import AccomdationList from '@/app/components/Accomdation/AccomdationList';
 
 export default function AccomdationContents() {
   const { setCurrentPage } = useInteractionStore();
   const [selectedArea, setSelectedArea] = useState<string>('전국');
-  const [buttonStates, setButtonStates] = useState<{ [key: string]: boolean }>({ 전국: true });
 
   const { data: stayData, isLoading, error } = useStayData(selectedArea);
 
-  const [processedStayData, setProcessedStayData] = useState<StayItem[]>([]);
-
-  useEffect(() => {
-    if (stayData) {
-      const processed = stayData.map((stay) => ({
-        ...stay,
-        addr1: stay.addr1.split(' ').slice(0, 2).join(' '), // 주소의 첫 두 부분만 사용
-        title: stay.title.length > 12 ? stay.title.slice(0, 12) + '...' : stay.title, // 12자로 제한
-      }));
-      setProcessedStayData(processed);
-    }
-  }, [stayData]);
-
-  useEffect(() => {
-    setButtonStates({ [selectedArea]: true });
-  }, [selectedArea]);
-
-  const handleLocationClick = (title: string) => {
-    setSelectedArea(title);
-    setButtonStates({ [title]: true });
-  };
+  const processedStayData = useStayProcessor(stayData);
 
   return (
     <section className="mt-6 1xl:mx-auto 1xl:max-w-[1000px]">
@@ -72,21 +51,7 @@ export default function AccomdationContents() {
 
       {/* 지역 선택 UI */}
       <div className="mt-6 pl-4 lg:px-6">
-        <Swiper slidesPerView="auto" spaceBetween={10} wrapperTag="ul">
-          {AreaHeaderSlide.map((slide, index) => (
-            <SwiperSlide
-              key={index}
-              onClick={() => handleLocationClick(slide.title)}
-              className={`!w-auto cursor-pointer whitespace-nowrap rounded-full px-4 py-2 text-sm transition-all ${
-                buttonStates[slide.title]
-                  ? 'bg-black text-white shadow-md'
-                  : 'text-gray-600 border-gray-200 hover:bg-gray-100 border bg-white'
-              }`}
-            >
-              {slide.title}
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <AccomdationHeader selectedArea={selectedArea} onSelect={setSelectedArea} />
       </div>
 
       {/* 숙박 데이터 표시 */}
@@ -105,41 +70,7 @@ export default function AccomdationContents() {
         {!isLoading && !error && (!stayData || stayData.length === 0) && <AreaEmptyState />}
         {!isLoading && !error && stayData && stayData.length > 0 && (
           <>
-            <Swiper
-              className="cursor-pointer !pb-3 lg:!pb-5 1xl:!pb-7"
-              slidesPerView={1.2}
-              spaceBetween={20}
-              centeredSlides={true}
-              loop={processedStayData.length > 4}
-              onSlideChange={(swiper) => setCurrentPage(swiper.realIndex + 1)}
-              breakpoints={{
-                1024: { slidesPerView: 4 },
-              }}
-            >
-              {processedStayData.map((stay) => (
-                <SwiperSlide key={stay.contentid}>
-                  <div className="rounded-lg bg-white shadow-md">
-                    <div className="relative aspect-[4/3]">
-                      <Image
-                        className="h-full w-full"
-                        src={stay.firstimage || '/error/no-image.png'}
-                        alt={stay.title}
-                        width={500}
-                        height={333}
-                        priority
-                      />
-                    </div>
-                    <div className="p-4">
-                      <h2 className="text-primary mb-2 text-lg font-bold">{stay.title}</h2>
-                      <p className="text-gray-600 text-sm">
-                        {' '}
-                        {stay.addr1 || '주소를 준비중입니다.'}
-                      </p>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            <AccomdationList stays={processedStayData} onSlideChange={setCurrentPage} />
             <ProgressBar totalPages={stayData.length} />
           </>
         )}
