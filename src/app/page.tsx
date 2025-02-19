3c28f5ce337a60fbb2516f608a16d0cd61bea40a
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-19 19:00:59 +0900

    feat : 메인페이지 부모 컴퍼넌트에 행사관련 컨텐츠 컴퍼넌트 추가완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import MainSlide from '@/app/components/Slide/MainSlide';
 import AccomdationContents from '@/app/components/Accomdation/AccomdationContents';
+import EventContents from '@/app/components/Event/EventContents';
 
 export default function HomePage() {
   return (
     <>
       <MainSlide />
       <AccomdationContents />
+      <EventContents />
     </>
   );
 }
