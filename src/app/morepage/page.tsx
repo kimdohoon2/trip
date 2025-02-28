47ce464d5d4d86e695d04dca3ae6123345579e9b
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-28 17:06:08 +0900

    feat : 더보기페이지 추가완료

---
+++ src/app/morepage/page.tsx
@@ -0,0 +1,11 @@
+import MoreHeader from '@/app/components/MorePage/MoreHeader';
+import MoreContents from '@/app/components/MorePage/MoreContents';
+
+export default function MorePage() {
+  return (
+    <>
+      <MoreHeader />
+      <MoreContents />
+    </>
+  );
+}
