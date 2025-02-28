9f41b80fb213a180eb0b11e7354f1680f9068ce6
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-02-28 17:17:31 +0900

    bug : http->https로 변경(배포하는 과정에서 생긴문제)

--- src/app/constant/apiConstants.ts
+++ src/app/constant/apiConstants.ts
@@ -3,6 +3,6 @@ export const TOUR_API_KEY = process.env.NEXT_PUBLIC_TOUR_API_KEY;
 export const API_URLS = {
   TOUR_LIST: 'https://apis.data.go.kr/B551011/KorService1/areaBasedList1',
   LOCATION_BASED: 'https://apis.data.go.kr/B551011/KorService1/locationBasedList1',
-  STAY_BASED: 'http://apis.data.go.kr/B551011/KorService1/searchStay1',
-  EVENT_BASED: 'http://apis.data.go.kr/B551011/KorService1/searchFestival1',
+  STAY_BASED: 'https://apis.data.go.kr/B551011/KorService1/searchStay1',
+  EVENT_BASED: 'https://apis.data.go.kr/B551011/KorService1/searchFestival1',
 };
