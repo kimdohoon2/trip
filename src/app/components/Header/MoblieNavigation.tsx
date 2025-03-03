de1a29caa4562009aa2f4e80ae38df2602633628
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-04 01:25:18 +0900

    feat : 하단 내비게이션 아이콘추가 기능 추가완료

--- src/app/components/Header/MoblieNavigation.tsx
+++ src/app/components/Header/MoblieNavigation.tsx
@@ -1,47 +1,78 @@
 import Link from 'next/link';
-export default function MoblieNavigation() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faHouseChimney, faMap, faUpLong } from '@fortawesome/free-solid-svg-icons';
+import SearchModal from '@/app/components/Header/SearchModal';
+import { useSearch } from '@/app/hooks/useSearch';
+
+export default function MobileNavigation() {
+  const {
+    keyword,
+    setKeyword,
+    isModalOpen,
+    openModal,
+    closeModal,
+    recentSearches,
+    handleSearch,
+    handleRecentSearchClick,
+    clearRecentSearches,
+    removeRecentSearch,
+  } = useSearch();
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth', // 부드럽게 스크롤
+      behavior: 'smooth',
     });
   };
+
   return (
     <nav>
       <ul className="flex items-center justify-between lg:w-full lg:gap-[50px]">
         <li className="w-[25%] text-center lg:w-auto">
           <Link
             href="/"
-            className="nav-affter relative block w-full py-[10px] text-[14px] lg:text-[20px]"
+            className="relative flex w-full flex-col gap-1 py-[10px] text-[14px] lg:text-[20px]"
           >
-            홈
+            <FontAwesomeIcon icon={faHouseChimney} /> <span>홈</span>
           </Link>
         </li>
         <li className="w-[25%] text-center lg:w-auto">
-          <Link
-            href="/area"
-            className="nav-affter relative block w-full py-[10px] text-[14px] lg:text-[20px]"
+          <button
+            onClick={openModal}
+            className="relative flex w-full flex-col gap-1 py-[10px] text-[14px] lg:text-[20px]"
           >
-            지역
-          </Link>
+            <FontAwesomeIcon icon={faSearch} /> <span>검색</span>
+          </button>
         </li>
         <li className="w-[25%] text-center lg:w-auto">
           <Link
             href="/morepage"
-            className="nav-affter relative block w-full py-[10px] text-[14px] lg:text-[20px]"
+            className="relative flex w-full flex-col gap-1 py-[10px] text-[14px] lg:text-[20px]"
           >
-            여행정보
+            <FontAwesomeIcon icon={faMap} /> <span>여행정보</span>
           </Link>
         </li>
         <li className="w-[25%] text-center lg:w-auto">
           <button
             onClick={scrollToTop}
-            className="nav-affter relative block w-full py-[10px] text-[14px] lg:text-[20px]"
+            className="relative flex w-full flex-col gap-1 py-[10px] text-[14px] lg:text-[20px]"
           >
-            up
+            <FontAwesomeIcon icon={faUpLong} /> <span>TOP</span>
           </button>
         </li>
       </ul>
+      {isModalOpen && (
+        <SearchModal
+          keyword={keyword}
+          setKeyword={setKeyword}
+          handleSearch={handleSearch}
+          recentSearches={recentSearches}
+          handleRecentSearchClick={handleRecentSearchClick}
+          clearRecentSearches={clearRecentSearches}
+          removeRecentSearch={removeRecentSearch}
+          onClose={closeModal}
+        />
+      )}
     </nav>
   );
 }
