0bc5c6a20a1e2f1b6a989165f335014987be3303
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-03 14:28:51 +0900

    feat : 화면에 따른 ui변경 추가완료

--- src/app/components/Search/SearchSection.tsx
+++ src/app/components/Search/SearchSection.tsx
@@ -1,8 +1,12 @@
-import { useState, useEffect } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import { SearchApiResponse } from '@/app/types/ItemType';
 import { filterAddress, filterTitle } from '@/app/utils/filterDate';
 import SearchPagination from './SearchPagination';
+import createKakaoMapURL from '@/app/utils/createKakaoMapURL';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 interface SearchSectionProps {
   contentTypeId: string;
@@ -12,9 +16,6 @@ interface SearchSectionProps {
   onExpand: (contentTypeId: string) => void;
 }
 
-const INITIAL_ITEMS = 5;
-const ITEMS_PER_PAGE = 10;
-
 export default function SearchSection({
   contentTypeId,
   name,
@@ -23,6 +24,27 @@ export default function SearchSection({
   onExpand,
 }: SearchSectionProps) {
   const [currentPage, setCurrentPage] = useState(1);
+  const [initialItems, setInitialItems] = useState(5);
+  const ITEMS_PER_PAGE = 10;
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (typeof window !== 'undefined') {
+        if (window.innerWidth >= 1024) {
+          setInitialItems(8);
+        } else if (window.innerWidth >= 768) {
+          setInitialItems(6);
+        } else {
+          setInitialItems(5);
+        }
+      }
+    };
+
+    handleResize(); // 초기 실행
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   useEffect(() => {
     if (!isExpanded) {
@@ -32,7 +54,7 @@ export default function SearchSection({
 
   const totalPages = Math.ceil(items.length / ITEMS_PER_PAGE);
   const startIndex = isExpanded ? (currentPage - 1) * ITEMS_PER_PAGE : 0;
-  const endIndex = isExpanded ? startIndex + ITEMS_PER_PAGE : INITIAL_ITEMS;
+  const endIndex = isExpanded ? startIndex + ITEMS_PER_PAGE : initialItems;
   const currentItems = items.slice(startIndex, endIndex);
 
   const handlePageChange = (page: number) => {
@@ -44,13 +66,13 @@ export default function SearchSection({
   };
 
   return (
-    <div className="mx-4 mb-4 rounded-2xl bg-white p-4 shadow-md">
+    <div className="mx-4 mb-4 rounded-2xl bg-white p-4 shadow-md lg:mb-8">
       <h2 className="mb-2 text-base font-semibold">{name}</h2>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         {currentItems.map((item: SearchApiResponse) => (
           <div key={item.contentid} className="border-b border-bordercolor">
             <div className="flex gap-4">
-              <div className="mb-2 h-16 w-24">
+              <div className="mb-2 h-16 w-24 lg:h-32 lg:w-44">
                 <Image
                   className="h-full w-full object-cover"
                   src={item.firstimage || '/error/no-image.png'}
@@ -59,15 +81,29 @@ export default function SearchSection({
                   height={225}
                 />
               </div>
-              <div>
-                <h3 className="text-base font-semibold">{filterTitle(item.title)}</h3>
-                <p className="text-sm">{filterAddress(item.addr1)}</p>
+              <div className="flex w-full justify-between">
+                <div>
+                  <h3 className="text-base font-semibold">{filterTitle(item.title)}</h3>
+                  <p className="text-sm">{filterAddress(item.addr1)}</p>
+                </div>
+                <div>
+                  <Link
+                    className="relative inline-block h-[30px] w-[30px] rounded-full border border-bordercolor text-sm hover:bg-black hover:text-white lg:text-base"
+                    href={createKakaoMapURL(item.addr1)}
+                    target="_blank"
+                  >
+                    <FontAwesomeIcon
+                      className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+                      icon={faMapLocationDot}
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         ))}
       </div>
-      {!isExpanded && items.length > INITIAL_ITEMS && (
+      {!isExpanded && items.length > initialItems && (
         <button
           onClick={handleShowMore}
           className="mt-4 w-full rounded bg-black px-4 py-2 text-center text-white"
