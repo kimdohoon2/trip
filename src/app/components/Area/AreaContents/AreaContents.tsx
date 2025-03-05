cb59b2afa104e5d3437333e8fb0d65d4f31c1b53
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-05 16:37:52 +0900

    refactoring : 스피너 관련 부분 위치 더보기 버튼 ui부분 위치 변경

--- src/app/components/Area/AreaContents/AreaContents.tsx
+++ src/app/components/Area/AreaContents/AreaContents.tsx
@@ -10,7 +10,7 @@ import CategorySelector from '@/app/components/Area/AreaContents/CategorySelecto
 import ProgressBar from '@/app/components/Common/ProgressBar';
 import MoreButton from '@/app/components/Common/MoreButton';
 import DataError from '@/app/components/Common/Error';
-import AreaEmptyState from '@/app/components/Common/AreaEmptyState';
+import EmptyState from '@/app/components/Common/EmptyState';
 import AreaSlideProps from '@/app/components/Area/AreaContents/AreaSlideProps';
 import { useAreaContentsLogic } from '@/app/hooks/useAreaContentsLogic';
 
@@ -25,16 +25,6 @@ export default function AreaContents() {
     isLoading,
     error,
   } = useAreaContentsLogic();
-
-  // 로딩 중일 때 표시할 UI
-  if (isLoading) {
-    return (
-      <div className="mt-32">
-        <Spinner />
-        <p className="text-center">잠시만 기다려 주세요.</p>
-      </div>
-    );
-  }
 
   // 에러 발생 시 표시할 UI
   if (error) {
@@ -51,9 +41,15 @@ export default function AreaContents() {
       <section className="relative -top-5 rounded-tl-[20px] bg-white pb-10 pl-4 pt-5 lg:-top-0 lg:px-6 lg:pb-12 lg:pt-12 1xl:m-auto 1xl:max-w-[1000px] 1xl:pl-0 1xl:pr-0">
         <h2 className="text-lg lg:text-2xl">{regionText}</h2>
         <CategorySelector availableCategories={availableCategories} />
-
-        {filteredData.length === 0 ? (
-          <AreaEmptyState />
+        {isLoading ? (
+          <div className="relative h-[400px] md:h-[670px]">
+            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+              <Spinner />
+              <p>잠시만 기다려 주세요 :)</p>
+            </div>
+          </div>
+        ) : filteredData.length === 0 ? (
+          <EmptyState />
         ) : (
           <>
             <Swiper
@@ -64,22 +60,22 @@ export default function AreaContents() {
                 1024: { slidesPerView: 4 },
               }}
               modules={[Grid]}
-              onSlideChange={handleSlideChange} // 슬라이드 변경 시 처리
+              onSlideChange={handleSlideChange}
               className="mt-[20px] h-full w-full lg:mt-[50px]"
             >
               {filteredData.map((item) => (
                 <SwiperSlide
-                  key={generateKey(item)} // 고유 키를 이용해 슬라이드 생성
+                  key={generateKey(item)}
                   className="flex w-full max-w-[300px] flex-row items-center justify-center pb-[15px] pr-[15px] lg:pb-[30px]"
                 >
-                  <AreaSlideProps item={item} /> {/* 슬라이드 아이템 */}
+                  <AreaSlideProps item={item} />
                 </SwiperSlide>
               ))}
             </Swiper>
-            <ProgressBar className="lg:hidden" totalPages={totalPages} /> {/* 진행 상태 바 */}
-            <MoreButton href="/morepage" text="더맣은" strongText="여행지" /> {/* 더 보기 버튼 */}
+            <ProgressBar className="lg:hidden" totalPages={totalPages} />
           </>
         )}
+        <MoreButton className="mt-4" href="/morepage" text="더많은" strongText="여행지" />
       </section>
     </>
   );
