b6c1a8b2361e4a429aa6da55a4c4ed0d94110063
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-06 22:45:45 +0900

    feat : 각각의 svg 컴퍼넌트 introContents에 추가완료
    - intro가 끝나고 contents 없어지는 애니메이션 추가완료
    - 세션스토리지에 저장하게 완료

---
+++ src/app/components/Intro/IntroContents.tsx
@@ -0,0 +1,92 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import CircleUp from '@/app/components/Intro/CircleUp';
+import CircleDown from '@/app/components/Intro/CircleDown';
+import LeftTopBar from '@/app/components/Intro/LeftTopBar';
+import LeftBottomBar from '@/app/components/Intro/LeftBottomBar';
+import RightBottomBar from '@/app/components/Intro/RightBottomBar';
+import RightTopBar from '@/app/components/Intro/RightTopBar';
+import LogoIcon from '@/app/components/Header/LogoIcon';
+import useSessionStorage from '@/app/hooks/useSessionStorage';
+import { SESSION_STORAGE_KEYS } from '@/app/constant/sessionStorageKeys';
+
+export default function IntroContents() {
+  const [hasSeenIntro, setHasSeenIntro] = useSessionStorage(
+    SESSION_STORAGE_KEYS.HAS_SEEN_INTRO,
+    false
+  );
+  const [animationState, setAnimationState] = useState('initial');
+
+  useEffect(() => {
+    if (!hasSeenIntro) {
+      const startTimer = setTimeout(() => {
+        setAnimationState('in');
+      }, 100);
+
+      const endTimer = setTimeout(() => {
+        setAnimationState('out');
+      }, 5000);
+
+      const completeTimer = setTimeout(() => {
+        setHasSeenIntro(true);
+      }, 6000);
+
+      return () => {
+        clearTimeout(startTimer);
+        clearTimeout(endTimer);
+        clearTimeout(completeTimer);
+      };
+    }
+  }, [hasSeenIntro, setHasSeenIntro]);
+
+  const getClipPathClass = () => {
+    switch (animationState) {
+      case 'initial':
+        return 'clip-circle-initial';
+      case 'in':
+        return 'clip-circle-in';
+      case 'out':
+        return 'clip-circle-out';
+      default:
+        return '';
+    }
+  };
+
+  if (hasSeenIntro) {
+    return null;
+  }
+
+  return (
+    <div className={`fixed left-0 top-0 z-50 h-[100vh] w-full bg-white ${getClipPathClass()}`}>
+      {/* <div className={`fixed left-0 top-0 z-50 h-[100vh] w-full bg-white`}> */}
+      <div className="relative top-1/2 mx-auto h-[240px] w-[360px] -translate-y-1/2 lg:h-[300px] lg:w-[400px]">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="relative h-[120px] w-[120px] rounded-full lg:h-[300px] lg:w-[300px]">
+            <div className="absolute left-0 top-0 h-[90px] w-[120px] lg:h-[225px] lg:w-[300px]">
+              <CircleUp />
+            </div>
+            <div className="absolute bottom-0 right-0 h-[90px] w-[120px] lg:h-[225px] lg:w-[300px]">
+              <CircleDown />
+            </div>
+          </div>
+        </div>
+        <div className="absolute left-1/4 top-1/4 flex h-[60px] w-[40px] -translate-x-1/2 -translate-y-1/2 rotate-[45deg] gap-1 lg:left-0 lg:top-0 lg:h-[100px] lg:w-[80px]">
+          <LeftTopBar />
+        </div>
+        <div className="absolute right-1/4 top-1/4 flex h-[60px] w-[40px] -translate-y-1/2 translate-x-1/2 rotate-[-45deg] gap-1 lg:right-0 lg:top-0 lg:h-[100px] lg:w-[80px]">
+          <RightTopBar />
+        </div>
+        <div className="absolute bottom-1/4 left-1/4 flex h-[60px] w-[40px] -translate-x-1/2 translate-y-1/2 rotate-[-45deg] gap-1 lg:bottom-0 lg:left-0 lg:h-[100px] lg:w-[80px]">
+          <LeftBottomBar />
+        </div>
+        <div className="absolute bottom-1/4 right-1/4 flex h-[60px] w-[40px] translate-x-1/2 translate-y-1/2 rotate-[45deg] gap-1 lg:bottom-0 lg:right-0 lg:h-[100px] lg:w-[80px]">
+          <RightBottomBar />
+        </div>
+      </div>
+      <div className="absolute left-1/2 top-[70%] h-10 w-[10rem] -translate-x-1/2 -translate-y-1/2 lg:top-1/2 lg:w-60">
+        <LogoIcon className="h-full w-full" />
+      </div>
+    </div>
+  );
+}
