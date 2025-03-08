5da4b574598e324c4aa10be8be3e7cf4108e5336
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-08 17:40:31 +0900

    refactoring : LightHouse 검사결과 접근성 개선을 위해 목록에 테그 'li'추가

--- src/app/components/Accomdation/AccomdationHeader.tsx
+++ src/app/components/Accomdation/AccomdationHeader.tsx
@@ -13,6 +13,7 @@ export default function AccomdationHeader({ selectedArea, onSelect }: Accomdatio
         <SwiperSlide
           key={slide.title}
           onClick={() => onSelect(slide.title)}
+          tag="li"
           className={`hover-button !w-auto cursor-pointer whitespace-nowrap rounded-full px-4 py-2 text-sm transition-all ${
             selectedArea === slide.title
               ? 'bg-black text-white shadow-md'
