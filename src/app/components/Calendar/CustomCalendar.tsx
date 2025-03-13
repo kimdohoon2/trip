746d7c6b693dcff6a816768ff6748f8be9524dfc
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-13 12:35:19 +0900

    feat : 캘린더 관련 x 아이콘 추가, 핸들링 추가완료

--- src/app/components/Calendar/CustomCalendar.tsx
+++ src/app/components/Calendar/CustomCalendar.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import DateDisplay from '@/app/components/Calendar/DateDisplay';
 import CalendarHeader from '@/app/components/Calendar/CalendarHeader';
 import CalendarGrid from '@/app/components/Calendar/CalendarGrid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 interface CustomCalendarProps {
   selectedDate: Date;
@@ -28,12 +30,20 @@ export default function CustomCalendar({ selectedDate, onDateChange }: CustomCal
     setCurrentMonth((prev) => (prev === 11 ? 0 : prev + 1));
     setCurrentYear((prev) => (currentMonth === 11 ? prev + 1 : prev));
   };
+  const handleClose = () => {
+    setShowCalendar(false);
+  };
 
   return (
     <div className="relative inline-block">
       <DateDisplay selectedDate={selectedDate} onClick={() => setShowCalendar(!showCalendar)} />
       {showCalendar && (
         <div className="absolute left-1/2 z-10 mt-2 w-64 -translate-x-1/2 rounded-lg bg-white p-4 shadow-lg">
+          <div className="flex justify-end">
+            <div className="cursor-pointer" onClick={handleClose}>
+              <FontAwesomeIcon icon={faXmark} />
+            </div>
+          </div>
           <CalendarHeader
             currentYear={currentYear}
             currentMonth={currentMonth}
