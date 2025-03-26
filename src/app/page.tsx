7f4d3b5ac103e3d73a2f68fcd77d55eed779dca6
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-03-26 19:10:18 +0900

    refactoring : 애니메이션 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { motion } from 'framer-motion';
 import MainSlide from '@/app/components/MainSlide/MainSlide';
 import AccomdationContents from '@/app/components/Accomdation/AccomdationContents';
 import EventContents from '@/app/components/Event/EventContents';
@@ -20,18 +19,13 @@ export default function HomePage() {
     <>
       <IntroContents onComplete={handleIntroComplete} />
       {showMainContent && (
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-        >
+        <>
           <Header />
           <MainSlide />
           <AccomdationContents />
           <EventContents />
           <Footer />
-        </motion.div>
+        </>
       )}
     </>
   );
